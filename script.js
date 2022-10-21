288d5e6112015b5b6d171360ab52a7958914ee30
Harry Watkins <harry.jw@outlook.com>
2022-10-21 14:21:20 +0100

    Add for loop to create 16x16 square divs

---
+++ script.js
@@ -0,0 +1,15 @@
+(function (){
+
+    "use strict"
+
+    const container = document.querySelector("#container");
+
+    for (let i = 0; i<16*16; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+    
+
+
+})()
