891d4ee038ce12d316e5aafe1acc51d7d7f5bcf6
Harry Watkins <harry.jw@outlook.com>
2022-10-23 21:46:57 +0100

    Change createSquares function so that number of squares can be changed

--- script.js
+++ script.js
@@ -4,18 +4,24 @@
 
     const container = document.querySelector("#container");
 
-    for (let i = 0; i<16*16; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
-
-        square.addEventListener("mouseover", fillSquare);
+    function createSquares (numSquares = 16) {
+        for (let i = 0; i<numSquares*numSquares; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = `${800/numSquares}px`;
+            square.style.width = `${800/numSquares}px`;
+            container.appendChild(square);
+    
+            square.addEventListener("mouseover", fillSquare);
+        }
     }
+
 
     function fillSquare (e) {
         e.target.classList.add("filled");
     }
     
+
 
 
 })()
